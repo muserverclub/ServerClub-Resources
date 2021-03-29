48eb49d90ce49ef56989b814cbdb5926442f4c47
muserverclub <80699118+muserverclub@users.noreply.github.com>
2021-03-29 20:43:23 -0300

    Update COMMAND AND NPC REQUERIMENTS DOCUMENT (por).lua

--- Guides (Guias)/Npc-Command-Requirement-Guide/COMMAND AND NPC REQUERIMENTS DOCUMENT (por).lua
+++ Guides (Guias)/Npc-Command-Requirement-Guide/COMMAND AND NPC REQUERIMENTS DOCUMENT (por).lua
@@ -1,45 +1,61 @@
-Command.Authority = {"Admin1","Admin2","Admin3"}                                                      -- Coloque o nome dos personagem que poderão usar esse comando ( outros personagens que não tiver inserido aqui, não poderão usar )
-Command.BlockMaps = {0,1,2}                                                                           -- Números dos mapas em que o script não funciona ( Se não estiver nada inserido aqui, qualquer mapa irá funcionar o comando )
-Command.AllowMaps = {}                                                                                -- Números dos mapas em que o comando funciona ( Se não estiver nada inserido aqui, qualquer mapa irá funcionar o comando )
-Command.BlockCoord = {0,1,2}                                                                          -- Coordenadas em que o script nÃ£o funciona ( Se nÃ£o estiver nada inserido aqui, qualquer Coordenadas irÃ¡ funcionar o comando )
-Command.AllowCoord = {}                                                                               -- Coordenadas dos mapas em que o comando funciona ( Se nÃ£o estiver nada inserido aqui, qualquer Coordenadas irÃ¡ funcionar o comando )
-Command.BlockDays = {}                                                                                -- Dias da semana que o script não funcionará ( 1 = Domingo, 7 = Sábado)
-Command.BlockHours = {}                                                                               -- Horas do dia que o script não funcionará ( 0 a 23 horas)
+Command.Authority = {"Admin1","Admin2","Admin3"}                                                      -- Coloque o nome dos personagem que poderÃ£o usar esse comando ( outros personagens que nÃ£o tiver inserido aqui, nÃ£o poderÃ£o usar )
+Command.BlockMaps = {0,1,2}                                                                           -- NÃºmeros dos mapas em que o script nÃ£o funciona ( Se nÃ£o estiver nada inserido aqui, qualquer mapa irÃ¡ funcionar o comando )
+Command.AllowMaps = {}                                                                                -- NÃºmeros dos mapas em que o comando funciona ( Se nÃ£o estiver nada inserido aqui, qualquer mapa irÃ¡ funcionar o comando )
+Command.BlockCoord = {
+	{
+		Map = 0,
+		MapXMin = 0,
+		MapYMin = 0,
+		MapXMax = 255,
+		MapYMax = 255
+	}
+}                                                                          -- Coordenadas em que o script nÃƒÂ£o funciona ( Se nÃƒÂ£o estiver nada inserido aqui, qualquer Coordenadas irÃƒÂ¡ funcionar o comando )
+Command.AllowCoord = {
+	{
+		Map = 0,
+		MapXMin = 0,
+		MapYMin = 0,
+		MapXMax = 255,
+		MapYMax = 255
+	}
+}                                                                               -- Coordenadas dos mapas em que o comando funciona ( Se nÃƒÂ£o estiver nada inserido aqui, qualquer Coordenadas irÃƒÂ¡ funcionar o comando )
+Command.BlockDays = {}                                                                                -- Dias da semana que o script nÃ£o funcionarÃ¡ ( 1 = Domingo, 7 = SÃ¡bado)
+Command.BlockHours = {}                                                                               -- Horas do dia que o script nÃ£o funcionarÃ¡ ( 0 a 23 horas)
 
-Command.CustomFunction = function(aIndex,Data)                                                        -- Função que será executada para fazer verificações extra
+Command.CustomFunction = function(aIndex,Data)                                                        -- FunÃ§Ã£o que serÃ¡ executada para fazer verificaÃ§Ãµes extra
 
-  --aIndex = Index do jogador principal que será verificado
-  --Data = retorna Index do NPC se estiver trabalhando com OnNpcTalk, ou um array com o código do comando na posição 1 e os argumentos do comando na posição 2, se estiver usando OnCommandManager
+  --aIndex = Index do jogador principal que serÃ¡ verificado
+  --Data = retorna Index do NPC se estiver trabalhando com OnNpcTalk, ou um array com o cÃ³digo do comando na posiÃ§Ã£o 1 e os argumentos do comando na posiÃ§Ã£o 2, se estiver usando OnCommandManager
 
-  --return 1          -- A verificação foi bem sucedida
-  --return 0          -- A verificação apresentou erro, e a função não irá prosseguir
+  --return 1          -- A verificaÃ§Ã£o foi bem sucedida
+  --return 0          -- A verificaÃ§Ã£o apresentou erro, e a funÃ§Ã£o nÃ£o irÃ¡ prosseguir
 
 end
 
-Command.AllowAccountLevel = {0,1,2,3}                                                                 -- Níveis de conta que pode usar o comando
+Command.AllowAccountLevel = {0,1,2,3}                                                                 -- NÃ­veis de conta que pode usar o comando
 Command.AllowClass = {0,1,2,3,4,5,6,7,8}                                                              -- Classes que podem usar o comando
-Command.AllowQuest = {0,1,2,3}                                                                        -- Níveis que podem usar o comando
-Command.AllowGens = {0,1,2}                                                                           -- Famílias Gens permitidas
-Command.AllowPKLevel = {0,1,2,3,4,5,6,7}                                                              -- Níveis de Status PK/Hero que podem usar o comando
+Command.AllowQuest = {0,1,2,3}                                                                        -- NÃ­veis que podem usar o comando
+Command.AllowGens = {0,1,2}                                                                           -- FamÃ­lias Gens permitidas
+Command.AllowPKLevel = {0,1,2,3,4,5,6,7}                                                              -- NÃ­veis de Status PK/Hero que podem usar o comando
 
 --[[
-	Todas as configurações a seguir são por vip!
+	Todas as configuraÃ§Ãµes a seguir sÃ£o por vip!
 	Command.Config = { free, vip1,vip2,vip3}
 ]]
-Command.CountDown = { 20, 20, 20, 20 }                                                                -- Tempo de delay em segundos para usar novamente esse comando ( para cada nível vip )
-Command.DayTimes = { 1, 1, 1, 1 }                                                                     -- Número de vezes que pode usar o comando diariamente com sucesso ( para cada nível vip )
-Command.AllowLevel = { {0,400}, {0,400}, {0,400}, {0,400} }                                           -- Minimo/Máximo Level para usar o comando ( para cada nível vip )
-Command.AllowMasterLevel = { {0,650}, {0,650}, {0,650}, {0,650} }                                     -- Minimo/Máximo Master Level para usar o comando ( para cada nível vip )
-Command.AllowReset = { {0,9999}, {0,9999}, {0,9999}, {0,9999} }                                       -- Minimo/Máximo Reset para usar o comando ( para cada nível vip )
-Command.AllowMasterReset = { {0,9999}, {0,9999}, {0,9999}, {0,9999} }                                 -- Minimo/Máximo Master Reset para usar o comando ( para cada nível vip )
-Command.AllowSafeZone = {true,true,true,true}                                                         -- Pode usar na area segura ( para cada nível vip )
-Command.AllowNotSafeZone = {false,true,true,true}                                                     -- Pode usar fora da area segura ( para cada nível vip )
+Command.CountDown = { 20, 20, 20, 20 }                                                                -- Tempo de delay em segundos para usar novamente esse comando ( para cada nÃ­vel vip )
+Command.DayTimes = { 1, 1, 1, 1 }                                                                     -- NÃºmero de vezes que pode usar o comando diariamente com sucesso ( para cada nÃ­vel vip )
+Command.AllowLevel = { {0,400}, {0,400}, {0,400}, {0,400} }                                           -- Minimo/MÃ¡ximo Level para usar o comando ( para cada nÃ­vel vip )
+Command.AllowMasterLevel = { {0,650}, {0,650}, {0,650}, {0,650} }                                     -- Minimo/MÃ¡ximo Master Level para usar o comando ( para cada nÃ­vel vip )
+Command.AllowReset = { {0,9999}, {0,9999}, {0,9999}, {0,9999} }                                       -- Minimo/MÃ¡ximo Reset para usar o comando ( para cada nÃ­vel vip )
+Command.AllowMasterReset = { {0,9999}, {0,9999}, {0,9999}, {0,9999} }                                 -- Minimo/MÃ¡ximo Master Reset para usar o comando ( para cada nÃ­vel vip )
+Command.AllowSafeZone = {true,true,true,true}                                                         -- Pode usar na area segura ( para cada nÃ­vel vip )
+Command.AllowNotSafeZone = {false,true,true,true}                                                     -- Pode usar fora da area segura ( para cada nÃ­vel vip )
 
-Command.ReqMoney = { 0, 0, 0, 0 }                                                                     -- Requerimento de Zen ( para cada nível vip )
-Command.ReqRuud = { 0, 0, 0, 0 }                                                                      -- Requerimento de Ruud ( para cada nível vip )
-Command.ReqWCoinC = { 0, 0, 0, 0 }                                                                    -- Requerimento de WCoinC ( para cada nível vip )
-Command.ReqWCoinP = { 0, 0, 0, 0 }                                                                    -- Requerimento de WCoinP ( para cada nível vip )
-Command.ReqGoblinPoint = { 0, 0, 0, 0 }                                                               -- Requerimento de GoblinPoint ( para cada nível vip )
+Command.ReqMoney = { 0, 0, 0, 0 }                                                                     -- Requerimento de Zen ( para cada nÃ­vel vip )
+Command.ReqRuud = { 0, 0, 0, 0 }                                                                      -- Requerimento de Ruud ( para cada nÃ­vel vip )
+Command.ReqWCoinC = { 0, 0, 0, 0 }                                                                    -- Requerimento de WCoinC ( para cada nÃ­vel vip )
+Command.ReqWCoinP = { 0, 0, 0, 0 }                                                                    -- Requerimento de WCoinP ( para cada nÃ­vel vip )
+Command.ReqGoblinPoint = { 0, 0, 0, 0 }                                                               -- Requerimento de GoblinPoint ( para cada nÃ­vel vip )
 
 --[[
   ReqItem = { Amount, Index, Level},
@@ -76,16 +92,16 @@ Command.ReqItem = {
   *** OPCIONAL ***
   MinLevel --> Item Min Level
   MaxLevel --> Item Max Level
-  Skill -- > Check Skill  (-1 ignorar, 0 não pode ter, 1 precisa ter)
-  Luck --> Check Luck  (-1 ignorar, 0 não pode ter, 1 precisa ter)
+  Skill -- > Check Skill  (-1 ignorar, 0 nÃ£o pode ter, 1 precisa ter)
+  Luck --> Check Luck  (-1 ignorar, 0 nÃ£o pode ter, 1 precisa ter)
   MinOption --> Min Item Option
   MaxOption --> Max Item Option
-  ExcType -- > Excelent Type (-1 ignora, 0 não pode, outro valor = soma dos EXC OPTION )
+  ExcType -- > Excelent Type (-1 ignora, 0 nÃ£o pode, outro valor = soma dos EXC OPTION )
   MinExc --> Min Exc
   MaxExc --> Max Exc
-  SetOption --> Set Item Option  (-1 ignorar, 0 não pode ter, 1 precisa ter)
-  JoHOption --> Jewel of Harmony  (-1 ignorar, 0 não pode ter, 1 precisa ter)
-  Option380 --> 380 Option  (-1 ignorar, 0 não pode ter, 1 precisa ter)
+  SetOption --> Set Item Option  (-1 ignorar, 0 nÃ£o pode ter, 1 precisa ter)
+  JoHOption --> Jewel of Harmony  (-1 ignorar, 0 nÃ£o pode ter, 1 precisa ter)
+  Option380 --> 380 Option  (-1 ignorar, 0 nÃ£o pode ter, 1 precisa ter)
   
 ]]
 Command.ReqItemEx = {
@@ -111,7 +127,7 @@ Command.QueryCharacter = {
     {
       "SELECT 1 FROM Character WHERE Name='%s' and ResetCount>'0'", -- VERIFICA
       "UPDATE Character SET ResetCount=ResetCount-1 WHERE Name='%s'", -- EXECUTA 
-      {"You need to have 1 Reset","Você precisa ter 1 Reset","Necesita tener 1 Reset"} -- AVISA CASO VERIFICAÇÃO FALHE
+      {"You need to have 1 Reset","VocÃª precisa ter 1 Reset","Necesita tener 1 Reset"} -- AVISA CASO VERIFICAÃ‡ÃƒO FALHE
     }
   },
   
@@ -119,7 +135,7 @@ Command.QueryCharacter = {
     {
       "SELECT 1 FROM Character WHERE Name='%s' and ResetCount>'0'",
       "UPDATE Character SET ResetCount=ResetCount-1 WHERE Name='%s'",
-      {"You need to have 1 Reset","Você precisa ter 1 Reset","Necesita tener 1 Reset"}
+      {"You need to have 1 Reset","VocÃª precisa ter 1 Reset","Necesita tener 1 Reset"}
     }
   },
   
@@ -127,7 +143,7 @@ Command.QueryCharacter = {
     {
       "SELECT 1 FROM Character WHERE Name='%s' and ResetCount>'0'",
       "UPDATE Character SET ResetCount=ResetCount-1 WHERE Name='%s'",
-      {"You need to have 1 Reset","Você precisa ter 1 Reset","Necesita tener 1 Reset"}
+      {"You need to have 1 Reset","VocÃª precisa ter 1 Reset","Necesita tener 1 Reset"}
     }
   },
   
@@ -135,7 +151,7 @@ Command.QueryCharacter = {
     {
       "SELECT 1 FROM Character WHERE Name='%s' and ResetCount>'0'",
       "UPDATE Character SET ResetCount=ResetCount-1 WHERE Name='%s'",
-      {"You need to have 1 Reset","Você precisa ter 1 Reset","Necesita tener 1 Reset"}
+      {"You need to have 1 Reset","VocÃª precisa ter 1 Reset","Necesita tener 1 Reset"}
     }
   }
 }
@@ -144,28 +160,28 @@ Command.QueryAccount = {
     {
       "SELECT 1 FROM CashShopData WHERE AccountId='%s' and GoblinPoint > 0",
       "UPDATE CashShopData SET GoblinPoint=GoblinPoint-1 WHERE AccountId='%s'",
-      {"You need to have 1 GoblinPoint","Você precisa ter 1 GoblinPoint","Necesita tener 1 GoblinPoint"}
+      {"You need to have 1 GoblinPoint","VocÃª precisa ter 1 GoblinPoint","Necesita tener 1 GoblinPoint"}
     }
   },
   {
     {
       "SELECT 1 FROM CashShopData WHERE AccountId='%s' and GoblinPoint > 0",
       "UPDATE CashShopData SET GoblinPoint=GoblinPoint-1 WHERE AccountId='%s'",
-      {"You need to have 1 GoblinPoint","Você precisa ter 1 GoblinPoint","Necesita tener 1 GoblinPoint"}
+      {"You need to have 1 GoblinPoint","VocÃª precisa ter 1 GoblinPoint","Necesita tener 1 GoblinPoint"}
     }
   },
   {
     {
       "SELECT 1 FROM CashShopData WHERE AccountId='%s' and GoblinPoint > 0",
       "UPDATE CashShopData SET GoblinPoint=GoblinPoint-1 WHERE AccountId='%s'",
-      {"You need to have 1 GoblinPoint","Você precisa ter 1 GoblinPoint","Necesita tener 1 GoblinPoint"}
+      {"You need to have 1 GoblinPoint","VocÃª precisa ter 1 GoblinPoint","Necesita tener 1 GoblinPoint"}
     }
   },
   {
     {
       "SELECT 1 FROM CashShopData WHERE AccountId='%s' and GoblinPoint > 0",
       "UPDATE CashShopData SET GoblinPoint=GoblinPoint-1 WHERE AccountId='%s'",
-      {"You need to have 1 GoblinPoint","Você precisa ter 1 GoblinPoint","Necesita tener 1 GoblinPoint"}
+      {"You need to have 1 GoblinPoint","VocÃª precisa ter 1 GoblinPoint","Necesita tener 1 GoblinPoint"}
     }
   }
 }
